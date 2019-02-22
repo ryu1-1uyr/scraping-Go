8a560f10cddbc78135e6d8140d9d9bdcc2d4a42f
s17001 <s17001@std.it-college.ac.jp>
2019-02-22 15:04:13 +0900

    first commit

---
+++ localserver.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World")      // Hello, Worldってアクセスした人に返すよ！
+}
+
+func main() {
+    http.HandleFunc("/", handler)       // http://localhost:8080/にアクセスしてきた人はhandlerを実行するよ！
+    fmt.Printf("server is running\n")
+    http.ListenAndServe(":8080", nil)   // サーバーを起動するよ！
+}
